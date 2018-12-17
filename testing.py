6925acd5b334638e4916f5c5e9f92dcb0f51bf27
tqr <mr.teo.qing.ren@gmail.com>
2018-12-17 10:59:49 +0800

    testing updates

--- testing.py
+++ testing.py
@@ -5,3 +5,15 @@ print(a+b)
 
 c = list(zip(a,b))
 print("hello")
+
+print(c)
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3], [1,2,3])
+
+import numpy as np
+
+x = np.linspace(0, 2*np.pi,1000)
+y = np.sin(x)
+plt.plot(x,y)
+
